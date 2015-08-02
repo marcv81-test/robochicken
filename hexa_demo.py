66aebd33c7253fff99f29018cc5be252c9ce1810
Marc <marc.venturini@gmail.com>
2015-08-02 22:57:35 +0800

    Jostick controlled demo

--- hexa_demo.py
+++ hexa_demo.py
@@ -1,6 +1,9 @@
 import math
 from kinematics import *
 from kinematics_visual import *
+import joystick
+
+joystick = joystick.Joystick("/dev/input/js0")
 
 legs = [None] * 6
 for i in range(6):
@@ -8,26 +11,30 @@ for i in range(6):
     legs[i] = VisualLimb(initial_rigid_motion = RigidMotion(initial_rotation, [1, 0, 1]))
 
 legs_baseline_position = [None] * 6
-baseline_position = RigidMotion(translation = [1.5, 0, -1.7])
+baseline_position = RigidMotion(translation = [2.2, 0, -1.2])
 for i in range(6):
     legs_baseline_position[i] = legs[i]._initial_rigid_motion.compose(baseline_position).translation_only()
 
 t = 0.0
-dt = 0.01
+dt = 0.04
 while True:
-    visual.rate(100)
+    visual.rate(25)
+    joystick.update()
     t += dt
-    displacement_x = 0.75 * math.sin(5 * t)
-    displacement_z = 0.5 * math.cos(5 * t)
+    displacement_x = 0.5 * joystick.axis_states["rx"]
+    displacement_y = 0.5 * joystick.axis_states["ry"]
+    displacement_z = 0.8 * joystick.axis_states["y"]
     factor = 1.0
     for i in range(6):
         target_position = list(legs_baseline_position[i])
         if (i % 2 == 0):
             target_position[0] = target_position[0] + displacement_x
+            target_position[1] = target_position[1] + displacement_y
             if (displacement_z > 0):
                 target_position[2] = target_position[2] + displacement_z
         else:
             target_position[0] = target_position[0] - displacement_x
+            target_position[1] = target_position[1] - displacement_y
             if (displacement_z < 0):
                 target_position[2] = target_position[2] - displacement_z
         legs[i].inverse_kinematics(target_position)
