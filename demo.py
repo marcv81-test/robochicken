c85c0c8c76bc2490df87b33c2c0fae44f05d1785
Marc <marc.venturini@gmail.com>
2015-08-04 23:03:22 +0800

    Select model from command-line arguments

---
+++ demo.py
@@ -0,0 +1,44 @@
+import math
+import argparse
+from visual import *
+from kinematics import *
+from kinematics_visual import *
+import joystick
+
+parser = argparse.ArgumentParser(description = "Display a joystick-controlled multipod.")
+parser.add_argument("--model", help = "hexapod by default, or 'spider'")
+args = parser.parse_args()
+
+scene.range = 5
+scene.forward = [1, 0, 0]
+scene.up = [0, 0, -1]
+arrow(axis = [1, 0, 0], color = color.red)
+arrow(axis = [0, 1, 0], color = color.green)
+arrow(axis = [0, 0, 1], color = color.blue)
+
+joystick = joystick.Joystick("/dev/input/js0")
+
+if (args.model == "spider"):
+    multipod = VisualMultipod(
+            leg_kwargs = {
+                "lengths": [0.25, 1, 2, 0.5],
+                "axes": [[0, 0, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0]],
+                "angles_limits": [
+                    [-tau / 8, tau / 8],
+                    [-3 * tau / 8, tau / 8],
+                    [0 * tau / 8, 4 * tau / 8],
+                    [-tau / 8, tau / 16]]},
+            legs_count = 8)
+else:
+    multipod = VisualMultipod()
+
+t = 0.0
+dt = 0.04
+while True:
+    rate(25)
+    t += dt
+    joystick.update()
+    x = -0.4 * joystick.axis_states["ry"]
+    y = 0.4 * joystick.axis_states["rx"]
+    z = 0.2 * (joystick.axis_states["y"] + 1.0)
+    multipod.direct_control(x, y, z)
