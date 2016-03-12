045f60750e17611fcd30fad011a124056ecaf06e
Marc <marc.venturini@gmail.com>
2016-03-12 12:01:53 +0800

    Jacobian now accepts array-likes

--- robotics/jacobian.py
+++ robotics/jacobian.py
@@ -10,18 +10,24 @@ class JacobianSolver:
         """Constructor"""
         self._function = function
         self._max_input_fix = max_input_fix
-        self._max_output_error = max_output_error
-        self._input_delta = input_delta
+        if max_input_fix == None: self._max_input_fix = None
+        else: self._max_input_fix = float(max_input_fix)
+        if max_output_error == None: self._max_output_error = None
+        else: self._max_output_error = float(max_output_error)
+        self._input_delta = float(input_delta)
 
     def jacobian_transpose_matrix(self, input_vector, output_vector = None):
         """Jacobian transpose matrix of the function at the input vector"""
+        input_vector = np.asfarray(input_vector)
         if (output_vector == None):
             output_vector = self._function(input_vector)
+        output_vector = np.asfarray(output_vector)
         matrix = np.zeros((len(input_vector), len(output_vector)))
         for i in xrange(len(input_vector)):
-            altered_input_vector = np.array(input_vector)
+            altered_input_vector = input_vector.copy()
             altered_input_vector[i] += self._input_delta
             altered_output_vector = self._function(altered_input_vector)
+            altered_output_vector = np.asfarray(altered_output_vector)
             output_delta_vector = altered_output_vector - output_vector
             matrix[i] = output_delta_vector / self._input_delta
         return matrix
@@ -34,8 +40,11 @@ class JacobianSolver:
         """Attempt to calculate an improved input vector so that the
         output vector converges toward the target.
         """
+        input_vector = np.asfarray(input_vector)
+        target_output_vector = np.asfarray(target_output_vector)
         if (output_vector == None):
             output_vector = self._function(input_vector)
+        output_vector = np.asfarray(output_vector)
         matrix = self.solver_matrix(
                 input_vector = input_vector,
                 output_vector = output_vector)
@@ -49,6 +58,7 @@ class JacobianSolver:
     def _limit_component(vector, max_component):
         """Scale a vector so that no components exceeds a maximum"""
         if max_component == None: return
+        assert vector.dtype == np.float_
         highest_component = np.amax(np.absolute(vector))
         if highest_component > max_component:
             np.multiply(vector, max_component / highest_component, vector)
@@ -57,6 +67,7 @@ class JacobianSolver:
     def _limit_norm(vector, max_norm):
         """Scale a vector so that the norm does not exceed a maximum"""
         if max_norm == None: return
+        assert vector.dtype == np.float_
         norm = np.linalg.norm(vector)
         if norm > max_norm:
             np.multiply(vector, max_norm / norm, vector)
--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -4,71 +4,67 @@ import numpy.testing as npt
 
 from robotics.jacobian import *
 
-# Shortcut to create arrays of floats
-def np_af(x):
-    return np.array(x, dtype = np.float_)
-
 class JacobianMatrixTestCase(unittest.TestCase):
 
     def test_limit(self):
 
         # No change if the highest component is at the limit
-        vector = np_af((1, -2))
+        vector = np.array((1, -2), np.float_)
         JacobianSolver._limit_component(vector, 2)
         npt.assert_almost_equal((1, -2), vector)
 
         # Scaling if the highest component exceeds the limit
-        vector = np_af((1, -2))
+        vector = np.array((1, -2), np.float_)
         JacobianSolver._limit_component(vector, 1)
         npt.assert_almost_equal((0.5, -1), vector)
 
         # No change if the norm is at the limit
-        vector = np_af((-6, 8))
+        vector = np.array((-6, 8), np.float_)
         JacobianSolver._limit_norm(vector, 10)
         npt.assert_almost_equal((-6, 8), vector)
 
         # Scaling if the norm exceeds the limit
-        vector = np_af((-6, 8))
+        vector = np.array((-6, 8), np.float_)
         JacobianSolver._limit_norm(vector, 5)
         npt.assert_almost_equal((-3, 4), vector)
 
     def test_jacobian_matrix(self):
 
-        matrix = np_af(((1, 0, 3), (0, 2, 2), (1, 2, 1)))
+        matrix = ((1, 0, 3), (0, 2, 2), (1, 2, 1))
         f = lambda x: np.dot(matrix, x)
 
-        # The Jacobian matrix of a linear map is the matrix of the linear map itself
+        # The Jacobian matrix of a linear map is the matrix of the linear map
         f_solver = JacobianSolver(function = f)
         npt.assert_almost_equal(matrix, f_solver.jacobian_matrix(
-                input_vector = np_af((0, 0, 0))))
+                input_vector = (0, 0, 0)))
         npt.assert_almost_equal(matrix, f_solver.jacobian_matrix(
-                input_vector = np_af((1, 2, 3))))
+                input_vector = (1, 2, 3)))
 
 class JacobianInverseSolverTestCase(unittest.TestCase):
 
     def test_converge(self):
 
-        matrix = np_af(((1, 0, 3), (0, 2, 2), (1, 2, 1)))
+        matrix = ((1, 0, 3), (0, 2, 2), (1, 2, 1))
         f = lambda x: np.dot(matrix, x)
 
         # For a linear map, the solver converges in a single iteration
         f_solver = JacobianInverseSolver(function = f)
         npt.assert_almost_equal((1, 1, 0), f_solver.converge(
-                input_vector = np_af((0, 0, 0)),
-                target_output_vector = np_af((1, 2, 3))))
+                input_vector = (0, 0, 0),
+                target_output_vector = (1, 2, 3)))
 
 class DampedLeastSquaresSolverTest(unittest.TestCase):
 
     def test_converge(self):
 
-        matrix = np_af(((1, 0, 3), (0, 2, 2), (1, 2, 1)))
+        matrix = ((1, 0, 3), (0, 2, 2), (1, 2, 1))
         f = lambda x: np.dot(matrix, x)
 
         # For a linear map, the solver converges after enough iterations
         f_solver = DampedLeastSquaresSolver(function = f, constant = 1)
-        input_vector = np_af((0, 0, 0))
+        input_vector = (0, 0, 0)
         for n in range(50):
             input_vector = f_solver.converge(
                     input_vector = input_vector,
-                    target_output_vector = np_af((1, 2, 3)))
+                    target_output_vector = (1, 2, 3))
         npt.assert_almost_equal((1, 1, 0), input_vector)
