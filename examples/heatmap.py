5042816fd393ae81f97d68a16c21821751d93e56
Marc <marc.venturini@gmail.com>
2016-03-15 23:05:45 +0800

    Cosmetic changes to heatmap

--- examples/heatmap.py
+++ examples/heatmap.py
@@ -13,7 +13,6 @@ lookup_table = LookupTable(
             {'from': -5, 'to': 5, 'points': 11},
             {'from': -2, 'to': 2, 'points': 5}],
         output_size = 1)
-
 lookup_table.populate(test_function)
 
 x = np.linspace(-5.2, 5.2, 100)
@@ -29,17 +28,20 @@ def prepare_plot(function):
     return color
 
 # Plot original function
-f = prepare_plot(lambda input_vector: test_function(input_vector))
+f = prepare_plot(lambda input_vector:
+        test_function(input_vector))
 pylab.subplot(1, 3, 1)
 pylab.pcolormesh(x, y, f)
 
 # Plot nearest interpolation
-f_nearest = prepare_plot(lambda input_vector: lookup_table.get_nearest(input_vector))
+f_nearest = prepare_plot(lambda input_vector:
+        lookup_table.get_nearest(input_vector))
 pylab.subplot(1, 3, 2)
 pylab.pcolormesh(x, y, f_nearest)
 
 # Plot linear interpolation
-f_lerp = prepare_plot(lambda input_vector: lookup_table.get_lerp(input_vector))
+f_lerp = prepare_plot(lambda input_vector:
+        lookup_table.get_lerp(input_vector))
 pylab.subplot(1, 3, 3)
 pylab.pcolormesh(x, y, f_lerp)
 
